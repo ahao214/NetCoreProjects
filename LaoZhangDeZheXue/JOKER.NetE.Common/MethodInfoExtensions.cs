439f193afe681afddde55a805b71de5ef808f0b9
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-05 22:37:48 +0800

    扩展方法

--- LaoZhangDeZheXue/JOKER.NetE.Common/MethodInfoExtensions.cs
+++ LaoZhangDeZheXue/JOKER.NetE.Common/MethodInfoExtensions.cs
@@ -4,7 +4,11 @@ namespace JOKER.NetE.Common
 {
     public static class MethodInfoExtensions
     {
-
+        /// <summary>
+        /// 扩展方法
+        /// </summary>
+        /// <param name="method"></param>
+        /// <returns></returns>
         public static string GetFullName(this MethodInfo method)
         {
             if(method .DeclaringType== null)
