f9b269c1bc58e5f8e61ccf86ee1d22de0bd25c67
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-05 22:22:51 +0800

    部门服务类

---
+++ LaoZhangDeZheXue/JOKER.NetE.Service/DepartmentService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOKER.NetE.Service
+{
+    /// <summary>
+    /// 部门服务类
+    /// </summary>
+    public class DepartmentService
+    {
+
+    }
+}
