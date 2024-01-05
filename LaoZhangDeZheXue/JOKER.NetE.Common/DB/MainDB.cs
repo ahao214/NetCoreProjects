e387aa36d6bde4143b5729c192aa93ffe17ebe4e
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-05 21:37:45 +0800

    sqlsugar的使用

---
+++ LaoZhangDeZheXue/JOKER.NetE.Common/DB/MainDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOKER.NetE.Common.DB
+{
+    public static class MainDB
+    {
+        public static string CurrentDbConnId = "Main";
+
+    }
+}
