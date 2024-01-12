4d0117fad0ca7d022055e93c2e2f53d6966ca3bf
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 15:46:06 +0800

    返回信息

---
+++ EProductSystem/Product.WebAPI/Controllers/Response/ServiceResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace Product.WebAPI.Controllers.Response
+{
+    /// <summary>
+    /// 返回信息
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+
+        public bool Success { get; set; }
+
+        public string? Message { get; set; }
+
+    }
+}
