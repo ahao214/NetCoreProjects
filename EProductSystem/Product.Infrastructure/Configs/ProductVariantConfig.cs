5012ad8c946f95fa4b024c84e40f4273171bc81f
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 14:33:55 +0800

    add ProductVariantConfig

---
+++ EProductSystem/Product.Infrastructure/Configs/ProductVariantConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Product.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Infrastructure.Configs
+{
+    public class ProductVariantConfig : IEntityTypeConfiguration<ProductVariant>
+    {
+        public void Configure(EntityTypeBuilder<ProductVariant> builder)
+        {
+            builder.ToTable($"T_{nameof(ProductVariant)}");
+        }
+
+
+    }
+
+}
