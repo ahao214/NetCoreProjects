a4c5b9e983e4f8a756ce5ce5825774cf35312c68
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 13:01:28 +0800

    商品类型

---
+++ EProductSystem/Product.Domain/Entity/ProductType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Domain.Entity
+{
+    /// <summary>
+    /// 商品类型
+    /// </summary>
+    public class ProductType
+    {
+        public Guid Id { get; init; }
+        public string Name { get; private set; }   //基础版 Pro版 ProMax版
+
+        public ProductType()
+        {
+
+        }
+
+        public ProductType(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+
+    }
+}
