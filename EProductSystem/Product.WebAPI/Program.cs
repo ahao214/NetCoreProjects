8f60315cec7300def8550d4eb1250bad756ca33e
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 14:56:12 +0800

    数据库配置

--- EProductSystem/Product.WebAPI/Program.cs
+++ EProductSystem/Product.WebAPI/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Product.Infrastructure.Configs;
+using Product.Infrastructure.DBContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<ProductDbContext>(opt =>
+//{
+//    opt.UseNpgsql(builder.Configuration.GetSection("ConnStr").Value);
+//});
+
+builder.Services.AddDbContext<ProductDbContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
