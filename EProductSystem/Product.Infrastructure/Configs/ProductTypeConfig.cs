bb9b20165c6b2c1b019ace4777d0a22f0cc781c1
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 14:31:46 +0800

    add ProductTypeConfig

---
+++ EProductSystem/Product.Infrastructure/Configs/ProductTypeConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Product.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Infrastructure.Configs
+{
+    public class ProductTypeConfig:IEntityTypeConfiguration<ProductType>
+    {
+        public void Configure(EntityTypeBuilder <ProductType > builder)
+        {
+            builder.ToTable($"T_{nameof(ProductType)}");
+        }
+
+    }
+}
