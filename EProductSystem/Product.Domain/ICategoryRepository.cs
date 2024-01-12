9afd82a7f4a994990b943d0de52b38526e2310fb
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 15:03:38 +0800

    类别接口

---
+++ EProductSystem/Product.Domain/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Product.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Domain
+{
+    /// <summary>
+    /// 类别接口
+    /// </summary>
+    public interface ICategoryRepository
+    {
+        /// <summary>
+        /// 查询所有类别
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Category>> FindAllCategoriesAsync();
+    }
+}
