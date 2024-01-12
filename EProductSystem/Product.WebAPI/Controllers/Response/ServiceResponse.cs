49f3400c1fd639a23c3bb9d6902011a8307fd928
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 16:08:58 +0800

    CategoryController

--- EProductSystem/Product.WebAPI/Controllers/Response/ServiceResponse.cs
+++ EProductSystem/Product.WebAPI/Controllers/Response/ServiceResponse.cs
@@ -8,7 +8,7 @@
     {
         public T? Data { get; set; }
 
-        public bool Success { get; set; }
+        public bool Success { get; set; } = true;
 
         public string? Message { get; set; }
 
