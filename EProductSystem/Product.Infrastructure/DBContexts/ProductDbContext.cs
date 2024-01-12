06b2f29191cfbf5c562be6db4da1e4c9f4c9f559
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-12 14:40:07 +0800

    add ProductDbContext

---
+++ EProductSystem/Product.Infrastructure/DBContexts/ProductDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Domain.Entity;
+
+namespace Product.Infrastructure.DBContexts
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Domain.Entity.Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductVariant> ProductVariants { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        public ProductDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+
+    }
+}
