3cd028b4e47724c70b5fc6bcb50efb1505853ea8
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-14 22:26:32 +0800

    ITokenService

---
+++ EProductSystem/Common.Jwt/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Common.Jwt
+{
+    public interface ITokenService
+    {
+        string BuildToken(IEnumerable<Claim> claims, JwtSetting setting);
+    }
+}
