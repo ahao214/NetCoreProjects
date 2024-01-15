ccb0b3d6f754fa0b392ca6c99bcdcbb274373113
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-15 13:56:27 +0800

    IUserRepository

---
+++ EProductSystem/User.Domain/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Domain.ValueObject;
+
+namespace User.Domain
+{
+    public interface IUserRepository
+    {
+        Task<User.Domain.Entity.User> FindOneAsync(PhoneNumber phone);
+        Task<User.Domain.Entity.User> FindOneAsync(Guid userId);
+        Task AddNewLoginHistoryAsync(PhoneNumber phone, string message);
+        Task SavePhoneNumberCodeAsync(PhoneNumber phone, string code);
+        Task<string?> FindPhoneNumberCodeAsync(PhoneNumber phone);
+        Task PublishEventAsync(UserAccessResultEvent userAccessResultEvent);
+    }
+}
