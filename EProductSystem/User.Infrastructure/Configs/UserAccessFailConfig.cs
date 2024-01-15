66f884bdeb3ce7c295409b6d8f669e5bf58f5a1a
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-15 13:40:34 +0800

    UserAccessFailConfig

---
+++ EProductSystem/User.Infrastructure/Configs/UserAccessFailConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Domain.Entity;
+
+namespace User.Infrastructure.Configs
+{
+    public class UserAccessFailConfig : IEntityTypeConfiguration<UserAccessFail>
+    {
+        public void Configure(EntityTypeBuilder<UserAccessFail> builder)
+        {
+            builder.ToTable($"T_{nameof(UserAccessFail)}");
+
+            builder.Property("isLockOut");
+        }
+    }
+}
