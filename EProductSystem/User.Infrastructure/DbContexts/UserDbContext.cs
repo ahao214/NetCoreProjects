628d3e578b74477e79ee1ec81151c9bfade7a5e3
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-15 13:43:21 +0800

    UserDbContext

---
+++ EProductSystem/User.Infrastructure/DbContexts/UserDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Domain.Entity;
+
+namespace User.Infrastructure.DbContexts
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<Domain.Entity.User> Users { get; set; }
+        public DbSet<UserLoginHistory> UserLoginHistories { get; set; }
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+
+    }
+}
