3c77b38da6918e3f8b5039a53866ae26ab444cbd
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-15 15:14:51 +0800

    UserController

---
+++ EProductSystem/User.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Common.Jwt;
+using Microsoft.AspNetCore.Mvc;
+using User.Domain;
+using User.Infrastructure.DbContexts;
+using User.WebAPI.Controllers.Request;
+using User.WebAPI.Responses;
+
+namespace User.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly UserDbContext _dbContext;
+
+        public UserController(IUserRepository userRepository, UserDbContext dbContext)
+        {
+            _userRepository = userRepository;
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        [UnitOfWork(typeof(UserDbContext))]
+        [NotCheckJwtVersion]
+        public async Task<ActionResult<ServiceResponse<string>>> AddNewUser(AddUserRequest req)
+        {
+            ServiceResponse<string> resp = new ServiceResponse<string>();
+            if (await _userRepository.FindOneAsync(req.phone) != null)
+            {
+                resp.Success = false;
+                resp.Message = "手机号已存在";
+                return BadRequest(resp);
+            }
+            var user = new User.Domain.Entity.User(req.phone, req.name);
+            user.ChangePassword(req.password);
+            _dbContext.Users.Add(user);
+
+            return Ok(resp);
+        }
+
+
+    }
+}
