84587d81950e099c0faed46113e73d29446e6af8
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-15 13:55:58 +0800

    ISmsCodeSender

---
+++ EProductSystem/User.Domain/ISmsCodeSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User.Domain.ValueObject;
+
+namespace User.Domain
+{
+    public interface ISmsCodeSender
+    {
+        Task SendAsync(PhoneNumber phone, string code);
+    }
+}
