19be51ce16bd89216034d7c062b467adbc23a70f
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 17:54:06 +0800

    用户服务层

---
+++ YCodeBlogSystem/Joker.Net.BaseService/UserService.cs
@@ -0,0 +1,20 @@
+﻿using Joker.Net.IBaseRepository;
+using Joker.Net.IBaseService;
+using Joker.Net.Model;
+
+
+namespace Joker.Net.BaseService
+{
+    public  class UserService:BaseService <User>,IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            base._repository = userRepository;
+            _userRepository = userRepository;
+        }
+
+
+    }
+}
---
+++ YCodeBlogSystem/Joker.Net.IBaseService/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Joker.Net.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joker.Net.IBaseService
+{
+    public interface IUserService : IBaseService<User>
+    {
+    }
+}
