fd418da41c0eccfa52a619a57de9edf61a6f2be7
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 14:17:50 +0800

    SqlDbContext

---
+++ YCodeBlogSystem/Joker.Net.EFCoreEnvironment/DbContexts/SqlDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Joker.Net.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Joker.Net.EFCoreEnvironment.DbContexts
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Article> articles { get; set; }
+
+        public DbSet<ArticleType> articleTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // 把当前程序集中把当前程序集中实现了IEntityTypeConfiguration接口的实现类加载进来
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+
+    }
+}
