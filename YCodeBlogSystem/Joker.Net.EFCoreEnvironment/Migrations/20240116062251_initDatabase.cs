5fda671ec0fef787a15073c4313e51e182420116
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 14:25:58 +0800

    生成数据库

---
+++ YCodeBlogSystem/Joker.Net.EFCoreEnvironment/Migrations/20240116062251_initDatabase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Joker.Net.EFCoreEnvironment.Migrations
+{
+    /// <inheritdoc />
+    public partial class initDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "T_ArticleType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TypeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_ArticleType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "T_Article",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "Text", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TypeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ViewCount = table.Column<int>(type: "int", nullable: false),
+                    LikeCount = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_Article", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_T_Article_T_ArticleType_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "T_ArticleType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_Article_TypeId",
+                table: "T_Article",
+                column: "TypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "T_Article");
+
+            migrationBuilder.DropTable(
+                name: "T_ArticleType");
+        }
+    }
+}
