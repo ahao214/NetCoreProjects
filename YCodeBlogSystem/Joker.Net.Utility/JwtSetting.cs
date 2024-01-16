382beb791d3329179f1b27822776532d6c4bb8ac
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 19:33:36 +0800

    JwtSetting

---
+++ YCodeBlogSystem/Joker.Net.Utility/JwtSetting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joker.Net.Utility
+{
+    public  class JwtSetting
+    {
+        public string? SecKey { get; set; }
+
+        public int ExpireSeconds { get; set; }
+
+        public string? Issuer { get; set; }
+
+        public string? Audience { get; set; }
+
+
+
+    }
+}
