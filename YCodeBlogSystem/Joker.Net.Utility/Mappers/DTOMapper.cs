4fa83ed678b3667d4b9c8b0ca15fb1a4ea75c6f4
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 17:28:49 +0800

    DTOMapper

---
+++ YCodeBlogSystem/Joker.Net.Utility/Mappers/DTOMapper.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Joker.Net.Model;
+using Joker.Net.Model.DTO;
+
+
+namespace Joker.Net.Utility.Mappers
+{
+    public class DTOMapper : Profile
+    {
+        public DTOMapper()
+        {
+            //Article
+            base.CreateMap<Article, ArticleDTO>().ForMember(x => x.TypeName, opt =>
+            {
+                opt.MapFrom(src => src.Type.TypeName);
+            });
+
+            //ArticleType
+            base.CreateMap<ArticleType, ArticleTypeDTO>().ForMember(x => x.ArticleNames, opt =>
+            {
+                opt.MapFrom(src => src.Articles.Select(x => x.Title));
+            });
+        }
+
+    }
+}
