8b7824af0f5454493233b14083f23bf65549e3ea
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 16:55:09 +0800

    文章和文章类型控制器

---
+++ YCodeBlogSystem/Joker.Net.MyBlog/Controllers/ArticleController.cs
@@ -0,0 +1,97 @@
+﻿using Joker.Net.IBaseService;
+using Joker.Net.Model;
+using Joker.Net.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Joker.Net.MyBlog.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+
+        [HttpGet("Articles")]
+        public async Task<ActionResult<ApiResult>> GetArticles()
+        {
+            var data = await _articleService.FindAllAsync();
+            if (data.Count == 0)
+            {
+                return ApiResultHelper.Error("没有更多的文章");
+            }
+            return ApiResultHelper.Success(data);
+        }
+
+
+        [HttpPost("Create")]
+        public async Task<ActionResult<ApiResult>> CreateArticle(string title, string content, Guid TypeId)
+        {
+            Article article = new Article()
+            {
+                Title = title,
+                Content = content,
+                CreateTime = DateTime.Now,
+                TypeId = TypeId,
+                IsDeleted = false,
+                ViewCount = 0,
+                LikeCount = 0
+            };
+
+            var result = await _articleService.CreateAsync(article);
+            if (!result)
+            {
+                return ApiResultHelper.Error($"{title} 添加失败!服务器发生故障");
+            }
+            return ApiResultHelper.Success(result);
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<ActionResult<ApiResult>> Delete(Guid id)
+        {
+            Article article = await _articleService.FindOneAsync(id);
+            if (article == null)
+            {
+                return ApiResultHelper.Error("删除失败");
+            }
+            // 软删除
+            article.IsDeleted = true;
+
+            bool result = await _articleService.DeletedAsync(article);
+            if (!result)
+            {
+                return ApiResultHelper.Error("删除失败!服务器发生故障");
+            }
+
+            return ApiResultHelper.Success(result);
+        }
+
+        [HttpPut("Edit")]
+        public async Task<ActionResult<ApiResult>> Edit(Guid id, string title, string content, Guid typeId)
+        {
+            Article article = await _articleService.FindOneAsync(id);
+            if (article is null)
+            {
+                return ApiResultHelper.Error("修改失败");
+            }
+            article.Title = title;
+            article.Content = content;
+            article.TypeId = typeId;
+
+            var result = await _articleService.UpdateAsync(article);
+            if (!result)
+            {
+                return ApiResultHelper.Error("修改失败");
+            }
+            return ApiResultHelper.Success(result);
+        }
+
+    }
+}
---
+++ YCodeBlogSystem/Joker.Net.MyBlog/Controllers/ArticleTypeController.cs
@@ -0,0 +1,100 @@
+﻿using Joker.Net.IBaseService;
+using Joker.Net.Model;
+using Joker.Net.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Joker.Net.MyBlog.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ArticleTypeController : ControllerBase
+    {
+        private readonly IArticleTypeService _articleTypeService;
+
+        public ArticleTypeController(IArticleTypeService articleTypeService)
+        {
+            this._articleTypeService = articleTypeService;
+        }
+
+        [HttpGet("GetTypes")]
+        [Authorize]
+        public async Task<ActionResult<ApiResult>> GetTypes()
+        {
+            var data = await _articleTypeService.FindAllAsync();
+            if (data.Count == 0)
+            {
+                return ApiResultHelper.Error("没有更多的文章类型");
+            }
+
+            List<ArticleType> articleType = new List<ArticleType>();
+
+            return ApiResultHelper.Success(articleType);
+        }
+
+        [HttpPost("Create")]
+        [Authorize]
+        public async Task<ActionResult<ApiResult>> Create(string TypeName)
+        {
+            ArticleType type = new ArticleType()
+            {
+                TypeName = TypeName,
+                IsDeleted = false
+            };
+
+            var result = await _articleTypeService.CreateAsync(type);
+
+            if (!result)
+            {
+                return ApiResultHelper.Error($"{TypeName} 类型创建失败!");
+            }
+
+            return ApiResultHelper.Success(result);
+        }
+
+        [HttpDelete("Deleted")]
+        [Authorize]
+        public async Task<ActionResult<ApiResult>> Delete(Guid id)
+        {
+            var data = await _articleTypeService.FindOneAsync(id);
+            if (data is null)
+            {
+                return ApiResultHelper.Error($"删除失败");
+            }
+
+            data.IsDeleted = true;
+
+            var result = await _articleTypeService.DeletedAsync(data);
+
+            if (!result)
+            {
+                return ApiResultHelper.Error($"删除失败");
+            }
+
+            return ApiResultHelper.Success(result);
+        }
+
+        [HttpPut("Edit")]
+        [Authorize]
+        public async Task<ActionResult<ApiResult>> Edit(Guid id, string TypeName)
+        {
+            var data = await _articleTypeService.FindOneAsync(id);
+            if (data is null)
+            {
+                return ApiResultHelper.Error($"修改失败");
+            }
+
+            data.TypeName = TypeName;
+
+            var result = await _articleTypeService.UpdateAsync(data);
+
+            if (!result)
+            {
+                return ApiResultHelper.Error($"修改失败");
+            }
+            return ApiResultHelper.Success(result);
+        }
+
+    }
+}
