3141ee065cf8bdb82a0ae23f374f96afa05467f8
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 18:12:23 +0800

    DTOMapper

---
+++ YCodeBlogSystem/Joker.Net.Model/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joker.Net.Model.DTO
+{
+    public record UserDTO
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public List<string> ArticleNames { get; set; } = new List<string>();
+
+
+    }
+}
