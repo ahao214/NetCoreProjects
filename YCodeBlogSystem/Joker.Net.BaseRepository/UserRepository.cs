221ecf910dcfd25ac6d43ef2fe1975fcb06aa0f5
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 18:09:40 +0800

    UserRepository

--- YCodeBlogSystem/Joker.Net.BaseRepository/UserRepository.cs
+++ YCodeBlogSystem/Joker.Net.BaseRepository/UserRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Joker.Net.EFCoreEnvironment.DbContexts;
 using Joker.Net.IBaseRepository;
 using Joker.Net.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Joker.Net.BaseRepository
 {
@@ -14,7 +16,25 @@ namespace Joker.Net.BaseRepository
             _dbContext = dbContext;
         }
 
+        public override async Task<List<User>> FindAllAsync()
+        {
+            return await _dbContext.Users.Include(x => x.Articles).ToListAsync();
+        }
 
+        public override async Task<List<User>> FindAllAsync(Expression<Func<User, bool>> del)
+        {
+            return await _dbContext.Users.Where(del).Include(x => x.Articles).ToListAsync();
+        }
+
+        public override async Task<User> FindOneAsync(Guid id)
+        {
+            return await _dbContext.Users.Include(x => x.Articles).SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async override Task<User> FindOneAsync(Expression<Func<User, bool>> del)
+        {
+            return await _dbContext.Users.Include(x => x.Articles).SingleOrDefaultAsync(del);
+        }
 
     }
 }
