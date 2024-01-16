1d904199e3a5de3150d498d81cdc5f989fb4cf28
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 18:07:29 +0800

    Identity注入

--- YCodeBlogSystem/Joker.Net.EFCoreEnvironment/Migrations/20240116100450_addIdentity.Designer.cs
+++ YCodeBlogSystem/Joker.Net.EFCoreEnvironment/Migrations/20240116100450_addIdentity.Designer.cs
@@ -4,6 +4,7 @@ using Joker.Net.EFCoreEnvironment.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Joker.Net.EFCoreEnvironment.Migrations
 {
     [DbContext(typeof(SqlDbContext))]
-    partial class SqlDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240116100450_addIdentity")]
+    partial class addIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
