be4e0182b02a00e7831a05ee129c4d3e54b7a896
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 16:34:42 +0800

    Joker.Net.Utility

---
+++ YCodeBlogSystem/Joker.Net.Utility/ApiResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Joker.Net.Utility
+{
+    public record ApiResult
+    {
+        public int Code { get; set; }
+
+        public string? Message { get; set; }
+
+        public int Total { get; set; }
+
+        public dynamic? Data { get; set; }
+
+
+    }
+}
