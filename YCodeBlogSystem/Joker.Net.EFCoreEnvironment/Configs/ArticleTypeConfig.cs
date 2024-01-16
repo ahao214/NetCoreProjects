d6c67ec2dcb8502f88f6fb4deee2f0989f0fe680
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 14:11:13 +0800

    ArticleTypeConfig

---
+++ YCodeBlogSystem/Joker.Net.EFCoreEnvironment/Configs/ArticleTypeConfig.cs
@@ -0,0 +1,19 @@
+﻿
+using Joker.Net.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Joker.Net.EFCoreEnvironment.Configs
+{
+    public class ArticleTypeConfig : IEntityTypeConfiguration<ArticleType>
+    {
+        public void Configure(EntityTypeBuilder<ArticleType> builder)
+        {
+            //配置表名
+            builder.ToTable($"T_{nameof(ArticleType)}");
+
+            //配置全局筛选器
+            builder.HasQueryFilter(x => x.IsDeleted == false);
+        }
+    }
+}
