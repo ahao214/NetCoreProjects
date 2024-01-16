0edd6c6ad14aedb17bcd4bd1e58300b0b8881246
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-16 17:51:50 +0800

    用户仓储层

---
+++ YCodeBlogSystem/Joker.Net.BaseRepository/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Joker.Net.EFCoreEnvironment.DbContexts;
+using Joker.Net.IBaseRepository;
+using Joker.Net.Model;
+
+namespace Joker.Net.BaseRepository
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        private readonly SqlDbContext _dbContext;
+
+        public UserRepository(SqlDbContext dbContext)
+        {
+            base._db = dbContext;
+            _dbContext = dbContext;
+        }
+
+
+
+    }
+}
