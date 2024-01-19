fd22ddfd694bf5892a1a04ec0fb453f57cda4b09
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-19 14:37:53 +0800

    清空购物车

--- EProductSystem/Cart.WebAPI/Program.cs
+++ EProductSystem/Cart.WebAPI/Program.cs
@@ -6,6 +6,7 @@ using Common.Jwt;
 using Common.RabbitMQ;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -63,7 +64,21 @@ builder.Services.AddCors(opt =>
     });
 });
 
+// 在程序启动时订阅购物车清除消息队列
+var serviceProvider = builder.Services.BuildServiceProvider();
+var _rabbit = serviceProvider.GetService<IRabbitMqService>();
 
+_rabbit.SubscribeMessage("joker_shop_cart", "cart", "RemoveCart", (message) =>
+{
+    var dbContext = serviceProvider.GetService<CartDbContext>();
+    var dict = JsonSerializer.Deserialize<Dictionary<string, Guid>>(message);
+
+    var isExist = dbContext.CartItems.FirstOrDefault(x => x.ProductId == dict["ProductId"] && x.ProductTypeId == dict["ProductTypeId"] && x.UserId == dict["UserId"]);
+
+    dbContext.CartItems.Remove(isExist);
+
+    dbContext.SaveChanges();
+});
 
 var app = builder.Build();
 
